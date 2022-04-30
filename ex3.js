83df4bad4f8a988e9a6bb0d46ceb6a42e9e57cf0
kianika <mona_yaghoubi@gmail.com>
2022-04-30 22:23:26 +0200

    ex3 v.2 updated

--- ex3.js
+++ ex3.js
@@ -1,4 +1,21 @@
 //approach1
+
+// The first object parameter.
+
+let a = {
+    name: "Benny",
+    phone: "3325558745",
+    email: "benny@edabit.com"
+    }
+
+// The second object parameter.
+    
+    let b = {
+    name: "Jason",
+    phone: "9853759720",
+    email: "jason@edabit.com"
+    }
+    
 function equal(obj1 , obj2){
     let test = true;
     for( let item in obj1){
@@ -16,4 +33,4 @@ console.log(equal(a,b));
 /* function equal(obj1 , obj2){
     return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
-console.log( equal(a,b)); */
+console.log(equal(a,b)); */
