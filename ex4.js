2eacfef26db9d359572c3663a7fee90bca3a0cb9
kianika <mona_yaghoubi@gmail.com>
2022-04-30 02:24:50 +0200

    ex3 is done

---
+++ ex4.js
@@ -0,0 +1,29 @@
+
+ let getStudentTopNotes1 = [
+    {
+    id: 1,
+    name: "Jacek",
+    notes: [5, 3, 4, 2, 5, 5]
+    },
+    {
+    id: 2,
+    name: "Ewa",
+    notes: [2, 3, 3, 3, 2, 5]
+    },
+    {
+    id: 3,
+    name: "Zygmunt",
+          notes: []
+    }
+    ] 
+
+function getStudentTopNotes(array){
+    let arr = array.map(function(item){
+        // if (item.notes.length == 0){
+        //     return 0;
+        // }
+      return Math.max(...item.notes,0);
+    })
+    return arr;
+}
+console.log(getStudentTopNotes(getStudentTopNotes1)); 
